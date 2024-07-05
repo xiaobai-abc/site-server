bc77cd4c41e6d73bb1020554d488eb586cd5f4e1
不染纤尘醉一笑 <17615186105@163.com>
2024-07-05 17:05:49 +0800

    update

--- mongo/schema/copywriting.js
+++ mongo/schema/copywriting.js
@@ -1,14 +1,30 @@
 const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const writeSchema = new mongoose.Schema({
-  title: {
-    type: String,
+  id: {
+    type: Number,
     unique: true //字段是否唯一
   },
   title: String,
   describe: String,
-  video: String
+  video: String,
+  type: {
+    type: String,
+    enum: ["video", "image", "text"]
+  }
 });
+
+writeSchema.statics.getNextId = async function (name) {
+  const document = await this.findOneAndUpdate(
+    { _id: name },
+    { $inc: { id: 1 } },
+    { new: true, upsert: true }
+  );
+  return document.id;
+};
+
+// writeSchema.plugin(AutoIncrement, { inc_field: "id" });
 
 module.exports = {
   writeSchema
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,7 +9,11 @@ const writingModel = mongoose.model("Copywriting", writing.writeSchema);
 
 router.get("/", function (req, res) {
   // 这里理论是进不来的 当我啥都没说
-  res.send("this is home");
+  res.send({
+    code: 200,
+    data: null,
+    message: "this is home"
+  });
 });
 
 router.get("/home", async function (req, res) {
@@ -24,8 +28,64 @@ router.get("/home", async function (req, res) {
 });
 
 router.get("/write", async function (req, res) {
-  const data = await writingModel.find(void 0, { _id: false }).exec();
+  const { page = 1, pageSize = 10 } = req.query;
+  const data = await writingModel
+    .find(void 0, { _id: false })
+    .skip(page * pageSize - pageSize)
+    .limit(pageSize)
+    .lean();
+  const count = await writingModel.countDocuments().exec();
+  function truncateString(str, num) {
+    if (str.length <= num) {
+      return str;
+    }
+    return str.slice(0, num) + "...";
+  }
 
+  res.status(200).send({
+    code: 200,
+    data: {
+      list: data.map((item) => {
+        return {
+          ...item,
+          describe: truncateString(item.describe, 20)
+        };
+      }),
+      total: count
+    },
+    messgae: "success"
+  });
+});
+
+router.post("/write/add", async function (req, res) {
+  const params = req.body;
+  const coumaxIdDocnt = await writingModel.findOne().sort({ id: -1 }).exec();
+  const maxId = coumaxIdDocnt ? coumaxIdDocnt.id : 0;
+
+  const newWriting = new writingModel({
+    ...params,
+    id: maxId
+  });
+
+  try {
+    await newWriting.save();
+    res.send({
+      code: 200,
+      data: null,
+      messgae: "添加成功"
+    });
+  } catch (error) {
+    res.send({
+      code: 0,
+      data: error,
+      messgae: "添加失败"
+    });
+  }
+});
+
+router.get("/write/detail", async function (req, res) {
+  const { id } = req.query;
+  const data = await writingModel.findOne({ id }, { _id: false }).lean().exec();
   res.status(200).send({
     code: 200,
     data: data,
